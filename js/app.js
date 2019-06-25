6fc31fa17f19228772ed9e65af247d0983229c22
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-25 19:57:22 +0100

    Created function for finding longest word in a string

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function findLongestWord(string) {
+		
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		findLongestWord(event.target.children[0].value);
 	});
 }
