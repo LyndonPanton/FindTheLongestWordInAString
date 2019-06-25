1985758bfb14f10f6b79f5443aaa03ac1587ff0c
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-25 20:08:17 +0100

    Added logic for finding longest word in a string

--- js/app.js
+++ js/app.js
@@ -4,7 +4,17 @@ window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function findLongestWord(string) {
+		let longest = ["", 0];
+
+
+		string.split(" ").forEach(function(word) {
+			if (word.length > longest[1]) {
+				longest[0] = word;
+				longest[1] = word.length;
+			}
+		});
 		
+		return longest;
 	}
 
 	let form = document.getElementById("form");
